68f65c7b7e63284c071d953adffbefee011cbb42
Walter Lucas <wldmc321@gmail.com>
2026-08-17 10:22:47 -0300

    remove tailwind e adicionar js

---
+++ frontend/3-js/1-primeiro-codigo-js/exercicio-01.js
@@ -0,0 +1,20 @@
+// Para fazer comentários no JavaScript é necessário colocar duas barras "//".
+// Eles servem para explicar o código e comentar partes do código que não serão usadas naquele momento.
+
+// console é um objeto pronto do JavaScript.
+
+// O . serve para acessar o método do objeto pronto. Nesse caso, o método acessado é o log.
+
+// log é um método (função) do console que imprime mensagens.
+// Um método ou função sempre vem acompanhado dos ().
+// Nem todos os métodos ou funções precisam de um parâmetro dentro dos parênteses.
+
+// Os () são operadores de chamada: servem para chamar um método ou função.
+
+// String é o texto que será exibido entre as aspas duplas ("").
+
+// "" é uma string vazia: representa um texto sem caracteres, útil para inicializar variáveis de texto ou indicar ausência de valor textual.
+
+// O ; serve para marcar o fim de uma instrução. No JavaScript não é obrigatório, mas é uma boa prática colocá-lo no final.
+
+console.log("Olá, Mundo!");
