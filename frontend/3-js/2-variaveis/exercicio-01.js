bcbcdb7cf88f2c44d218b9b06f3ccbb82f3efb76
Walter Lucas <wldmc321@gmail.com>
2026-08-17 11:17:30 -0300

    exercicio-02.js

---
+++ frontend/3-js/2-variaveis/exercicio-01.js
@@ -0,0 +1,9 @@
+// Variáveis são caixas que guardam um valor, que pode ser uma string, number ou booleano,
+// e que serão usados em algum momento no código.
+// Elas também servem para reutilizar o valor delas várias vezes sem precisar digitá-lo toda hora.
+// Além de facilitar a manutenção: se for preciso mudar uma informação, mudamos em apenas um lugar.
+
+// Criando a primeira variável
+let saudacao = "Olá, Mundo!";
+
+console.log(saudacao);
---
+++ frontend/3-js/2-variaveis/exercicio-02.js
@@ -0,0 +1,9 @@
+// Mudando valores de uma variavel
+
+let saudacao = "Olá, Mundo!";
+
+
+// Nova saudação
+ saudacao = "Meu nome é Walte Lucas";
+
+console.log("Nova saudação: " + saudacao);
