298881e9bc14338da029fdf020841f6b9cf7a819
Walter Lucas <wldmc321@gmail.com>
2026-09-02 16:20:46 -0300

    aula 13. lage page estacao mare, no curso html

--- frontend/3-js/2-como-declara-variaveis/desafios/desafio-01.js
+++
@@ -1,8 +0,0 @@
-// A variavel pessoa recebe o valor Carlos que é uma string
-let pessoa = "Carlos";
-
-// Agora a varaivel mudou de valor. Antes Carlos agora Desenvolvedor.
-pessoa = "Desenvolvedor";
-
-// No terminal o Reslutado será Profissão: Desenvolvedor.
-console.log("Profissão: " + pessoa + ".");
